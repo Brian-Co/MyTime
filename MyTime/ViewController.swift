1103c7ce166402139353e75e57bfc3b5fcb15994
Brian-Co <brian.corrieri@gmail.com>
2020-01-04 16:15:38 +0100

    Initial Commit

---
+++ MyTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTime
+//
+//  Created by Brian Corrieri on 04/01/2020.
+//  Copyright © 2020 FairTrip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyTimeTests/MyTimeTests.swift
@@ -0,0 +1,34 @@
+//
+//  MyTimeTests.swift
+//  MyTimeTests
+//
+//  Created by Brian Corrieri on 04/01/2020.
+//  Copyright © 2020 FairTrip. All rights reserved.
+//
+
+import XCTest
+@testable import MyTime
+
+class MyTimeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
